09fadc29feaea74a19461f15db4262dcc9013004
Josh Nichols <josh@technicalpickles.com>
2009-07-30 21:37:31 -0400

    Switched apps_controller to resource_controller.

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,41 +1,8 @@
-class AppsController < ApplicationController
+class AppsController < ResourceController::Base
+  create.wants.html { redirect_to root_url }
 
-  def new
-    @app = App.new
-  end
+  destroy.flash 'App deleted.'
+  destroy.wants.html { redirect_to root_url }
 
-  def edit
-    @app = App.find params[:id]
-  end
-
-  def create
-    @app = App.new params[:app]
-
-    if @app.save
-      flash[:success] = 'App created.'
-      redirect_to root_path
-    else
-      render :action => 'new'
-    end
-  end
-
-  def update
-    @app = App.find params[:id]
-
-    if @app.update_attributes(params[:app])
-      flash[:sucess] = 'App updated.'
-      redirect_to app_path(@app)
-    else
-      render :action => 'edit'
-    end
-  end
-
-  def destroy
-    @app = App.find(params[:id])
-    @app.destroy
-
-    flash[:success] = 'App deleted.'
-    redirect_to root_path
-  end
 
 end
